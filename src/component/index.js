c39f5c92a468ed0e380159e619eb5789422aafc9
adityajain-web <pranay.mishra.del@gmail.com>
2023-12-13 23:29:47 +0530

    EMPTY

--- src/component/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HeaderOffCanvas } from './header-offcanvas'
-export { default as ContactForm } from './form/contact-form'
-export { default as CheckoutForm } from './form/checkoutForm'
-export { default as AccordionItem } from './accordion/accordion1'
