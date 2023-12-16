26c53108f6c7ee576658c44821739523975e7266
adityajain-web <pranay.mishra.del@gmail.com>
2023-12-16 20:54:10 +0530

    empty

--- src/sections/layout/nav-config/index.js
+++
@@ -1,8 +0,0 @@
-const menu = [
-    { id: 'menu-1', href: "/about/", text: 'About' },
-    { id: 'menu-2', href: "/services/", text: 'Services' },
-    { id: 'menu-3', href: "/pricing/", text: 'Pricing' },
-    { id: 'menu-4', href: "/blog/", text: 'Blog' },
-]
-
-export default menu
